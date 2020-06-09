4d335a7c5be5168a8d278cebac7c9285d85704f3
manishdsilva <manishdsilva7@gmail.com>
2020-06-09 21:43:18 +0530

    Newlook using Template Front End

--- carsforu/views.py
+++ carsforu/views.py
@@ -1,16 +1,20 @@
 from django.shortcuts import render
 from pandas import DataFrame
+from django.views.decorators.csrf import csrf_protect
 import joblib 
 
 
 def index(request):
     return render(request, 'carsforu/index.html', {})
 
+
+
+
 def submit_it(request):
-    budget = int(request.POST['budget'])
-    mileage = int(request.POST['mileage'])
-    performance = int(request.POST['performance'])
-    seats = int(request.POST['seats'])
+    budget = int(request.POST.get('budget'))
+    mileage = int(request.POST.get('mileage'))
+    performance = int(request.POST.get('performance'))
+    seats = int(request.POST.get('seats'))
     
     data = [[mileage,performance,seats,budget]]
     df_pred = DataFrame(data, columns = ['Mileage', 'Engine_Power', 'Number_of_seats','Price'])
@@ -20,4 +24,5 @@ def submit_it(request):
     
     suggested = str(y_pred)
     suggested = suggested[2:-2]
+    print(suggested)
     return render(request, 'carsforu/submit.html', {'suggested':suggested})
